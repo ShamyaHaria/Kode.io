df126f05177492a79764296f59fc753ba9986484
Shamya <shamyaharia@gmail.com>
2026-02-13 17:38:02 -0500

    Editor Added

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -2,33 +2,59 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { io } from 'socket.io-client';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import Editor from "@monaco-editor/react";
 
 function Room() {
     const { roomId } = useParams();
     const location = useLocation();
     const { username } = location.state;
+    const [code, setCode] = useState('');
+    const socketRef = useRef(null);
+
+     function handleEditorChange(newCode){
+        setCode(newCode);
+        socketRef.current.emit('code-change',{
+            roomId: roomId,
+            code: newCode
+        });
+    }
 
     useEffect(() => {
-        const socket = io('http://localhost:5001');
+        socketRef.current = io('http://localhost:5001');
         console.log('WebSocket Connected');
 
-        socket.emit('join-room', {
+        socketRef.current.emit('join-room', {
             roomId: roomId,
             username: username
         });
 
-        socket.on('load-code',(code) => {
-        console.log(code);
+        socketRef.current.on('load-code',(code) => {
+            console.log(code);
+            setCode(code);
         });
+
+        socketRef.current.on('code-update',(updatedCode)=>{
+            setCode(updatedCode);
+        })
     }, []);
 
     return (
         <div>
-            <h1>Room Component</h1>
-            <p>This is where the editor shall be.</p>
-            <p>Room Id: {roomId}</p>
-            <p>Username: {username}</p>
+            <div>
+                <h1>Room Component</h1>
+                <p>This is where the editor shall be.</p>
+                <p>Room Id: {roomId}</p>
+                <p>Username: {username}</p>
+            </div>
+
+        <Editor
+            height="500px"
+            defaultLanguage="javascript"
+            value={code}
+            theme="vs-dark"
+            onChange={handleEditorChange}
+        />
         </div>
     )
 }
